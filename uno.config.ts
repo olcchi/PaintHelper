a3f0b2c47c36e063fb383949e9b33bdd3e173f6e
ekar <earthb0y@outlook.com>
2025-02-19 01:10:21 +0800

    chore: preset icon

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from 'unocss'
-
+import { defineConfig,presetIcons,presetUno } from 'unocss'
 export default defineConfig({
-  // ...UnoCSS options
+  presets:[
+    presetUno(),
+    presetIcons()
+  ]
 })
